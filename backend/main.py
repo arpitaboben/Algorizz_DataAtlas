966639179020c61b000537db0ad7c27cca801c4d
Satyam Singh <satyamsingh.007g@gmail.com>
2026-04-05 12:42:36 +0530

    ....

--- backend/main.py
+++ backend/main.py
@@ -69,6 +69,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
+        "https://dataatlas-virid.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
